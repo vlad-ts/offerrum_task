89952525ac932a9348164f13f51edf0a81b8371a
vlad-ts <beattle4567@gmail.com>
2023-05-09 16:13:23 +0300

    Fixed svg import

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,5 @@
 import NFT from "./NFT.svg";
-import Man from "./Man1.svg";
+import Man from "./MAN1.svg";
 import EmojiStars from "./emoji_stars.svg";
 import EmojiSunglasses from "./emoji_sunglasses.svg";
 import EmojiDollars from "./emoji_dollars.svg";
